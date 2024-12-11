32901c12a28fc3ac8cb7cd945ad12d3146ab967c
hseyndinc <hseyndinc@gmail.com>
2024-12-11 21:52:04 +0100

    Testing - Example- Hseyndinc

---
+++ include/Calculator.h
@@ -0,0 +1,16 @@
+//
+// Created by Hseyn Dinc on 11.12.24.
+//
+
+#ifndef TASCHENRECHNER_CALCULATOR_H
+#define TASCHENRECHNER_CALCULATOR_H
+
+class Calculator {
+public:
+    double add(double a, double b);
+    double subtract(double a, double b);
+private:
+    // Hier könnten später private Variablen kommen
+};
+
+#endif //TASCHENRECHNER_CALCULATOR_H
---
+++ src/Calculator.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Hseyn Dinc on 11.12.24.
+//
+
+#include "../include/Calculator.h"
+
+double Calculator::add(double a, double b) {
+    return a + b;
+}
+
+double Calculator::subtract(double a, double b) {
+    return a - b;
+}
